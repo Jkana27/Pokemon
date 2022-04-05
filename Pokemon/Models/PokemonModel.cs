706e94a99e0a5b0ad5a040e2ea31d8f35658f3f4
JacobKana <jacob_kana@yahoo.com>
2022-04-05 13:21:54 -0500

    Add project files.

---
+++ Pokemon/Models/PokemonModel.cs
@@ -0,0 +1,37 @@
+﻿namespace Pokemon.Models
+{
+    public class PokemonModel
+    {
+        public int Height { get; set; }
+        public int Id { get; set; }
+        public bool Is_default { get; set; }
+        public string? location_area_encounters { get; set; }
+        public string Name { get; set; }
+        public int Order { get; set; }
+        public Species species { get; set; }
+        public Sprites sprites { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class Species
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class Sprites
+    {
+        public string back_default { get; set; }
+        public object back_female { get; set; }
+        public string back_shiny { get; set; }
+        public object back_shiny_female { get; set; }
+        public string front_default { get; set; }
+        public object front_female { get; set; }
+        public string front_shiny { get; set; }
+        public object front_shiny_female { get; set; }
+
+    }
+
+
+
+}
